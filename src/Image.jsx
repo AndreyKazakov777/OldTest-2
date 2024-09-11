7ac629c8f6d9848a7cf732d8974d2dac3b29fa93
Andrey <andey-superman2011@yandex.ru>
2024-09-11 17:15:45 +0300

    some changes

---
+++ src/Image.jsx
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+ function LinkTo() {
+  return (
+    
+   
+    <a 
+    href="https://reactjs.org"
+    target="_blank" ><img src={logo} alt='React_logo' width='200'/></a>   
+
+  
+
+ );
+} 
+
+export default LinkTo;
